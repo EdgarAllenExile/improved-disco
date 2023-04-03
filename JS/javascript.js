482558a9dbd39c9a99d2f5740bf76dc5f6e1afc0
Riley Mercer <riley.mercer4@gmail.com>
2023-04-03 14:42:06 +0800

    Committing to be able to send to Phoebe

--- JS/javascript.js
+++ JS/javascript.js
@@ -1,34 +1,37 @@
 ///////////// I should use comments and nice things here for my own benefit
+///// Global Variables
+let naughtScore = 0;
+let crossScore = 0;
+
 
 /////////////// on click logic for game /////////////////
 $(document).ready(function () {
-
-$('.game-button').on('click', function(){
-    $(this).html('X')
-    winChecker();
-});
-
-$('.game-button').on('contextmenu', function(){
-    $(this).html('O')
-    winChecker();
-    return false
-});
-
+    $('.game-button').on('click', function(){
+        $(this).html('X')
+        winChecker();
+    });
+    $('.game-button').on('contextmenu', function(){
+        $(this).html('O')
+        winChecker();
+        return false
+    });
 });
 
 const winChecker = function(){
     //// Checks Horizontal Wins
     if (($('#00').html() == $('#01').html() && $('#01').html() == $('#02').html()) || ($('#10').html() == $('#11').html() && $('#11').html() == $('#12').html()) || ($('#20').html() == $('#21').html() && $('#21').html() == $('#22').html())){
-        console.log("You Win Horizontally!")
+        alert("You Win Horizontally!")
     }   
     //// Checks Vertical Wins
     if (($('#00').html() == $('#10').html() && $('#10').html() == $('#20').html()) || ($('#01').html() == $('#11').html() && $('#11').html() == $('#21').html()) || ($('#02').html() == $('#21').html() && $('#21').html() == $('#22').html())){
-        console.log("You Win Vertically!")
+        alert("You Win Vertically!")
     }
     //// Checks Diagonal Wins
     if (($('#00').html() == $('#11').html() && $('#11').html() == $('#22').html()) || ($('#02').html() == $('#11').html() && $('#11').html() == $('#20').html())){
-        console.log("You Win Diagonally!")
+        alert("You Win Diagonally!")
     }
 }
 
-//Now we make it do something fun!
+// const scoreUpdate = function{
+
+// }
