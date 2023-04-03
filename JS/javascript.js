3bb7cc34bcf8b8d740729c78d22145954fba0f79
Riley Mercer <riley.mercer4@gmail.com>
2023-04-03 12:11:39 +0800

    basic template created

---
+++ JS/javascript.js
@@ -0,0 +1,11 @@
+///////////// I should use comments and nice things here for my own benefit
+
+/////////////// on click logic for game /////////////////
+$('.game-box').on('click', function(){
+    console.log("Hello World")
+})
+
+$('#00').on('click', function(){
+    console.log("Hello World")
+
+})
