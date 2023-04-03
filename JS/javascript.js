aa0a76730b2cbaaa0cbc7b49144086fa42a1939d
Riley Mercer <riley.mercer4@gmail.com>
2023-04-03 14:19:13 +0800

    basic template created

--- JS/javascript.js
+++ JS/javascript.js
@@ -1,11 +1,32 @@
 ///////////// I should use comments and nice things here for my own benefit
 
 /////////////// on click logic for game /////////////////
-$('.game-box').on('click', function(){
-    console.log("Hello World")
-})
+$(document).ready(function () {
 
-$('#00').on('click', function(){
-    console.log("Hello World")
+$('.game-button').on('click', function(){
+    $(this).html('X')
+    winChecker();
+});
 
-})
+$('.game-button').on('contextmenu', function(){
+    $(this).html('O')
+    winChecker();
+    return false
+});
+
+});
+
+const winChecker = function(){
+    //// Checks Horizontal Wins
+    if (($('#00').html() == $('#01').html() && $('#01').html() == $('#02').html()) || ($('#10').html() == $('#11').html() && $('#11').html() == $('#12').html()) || ($('#20').html() == $('#21').html() && $('#21').html() == $('#22').html())){
+        console.log("You Win Horizontally!")
+    }   
+    //// Checks Vertical Wins
+    if (($('#00').html() == $('#10').html() && $('#10').html() == $('#20').html()) || ($('#01').html() == $('#11').html() && $('#11').html() == $('#21').html()) || ($('#02').html() == $('#21').html() && $('#21').html() == $('#22').html())){
+        console.log("You Win Vertically!")
+    }
+    //// Checks Diagonal Wins
+    if (($('#00').html() == $('#11').html() && $('#11').html() == $('#22').html()) || ($('#02').html() == $('#11').html() && $('#11').html() == $('#20').html())){
+        console.log("You Win Diagonally!")
+    }
+}
