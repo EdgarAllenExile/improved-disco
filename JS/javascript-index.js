3014c93ae1f6d41519c9cf70a80e7480df05921e
Riley Mercer <riley.mercer4@gmail.com>
2023-04-04 16:27:29 +0800

    Supports Custom Player Colours and Symbols / Persistent Score Count / Board Resets. TBA AI Player

--- JS/javascript-index.js
+++ JS/javascript-index.js
@@ -1,12 +1,17 @@
 ///////////// I should use comments and nice things here for my own benefit
 
-// <<<<<<<<<<<<<<<<<<<< Menu Functionality >>>>>>>>>>>>>>>>>>>>>>>>>>>
+// // <<<<<<<<<<<<<<<<<<<< Menu Functionality >>>>>>>>>>>>>>>>>>>>>>>>>>>
 $(document).ready(function () { //////I am still not quite sure how this works. But wrapping everything in it does work...
 
-$('#single-player-button').on('click', function(){
-    <a href = "single-player.html"> Single-Player </a>
+$('#multi-player-button').on('click', function() {
+    let p1name = $('#player1-name').val()
+    let p2name = $('#player2-name').val()
+    let p1col = $('#player1-col').val()
+    let p2col = $('#player2-col').val()
+    sessionStorage.setItem("player1-name", p1name);
+    sessionStorage.setItem("player2-name", p2name);
+    sessionStorage.setItem("player1-col", p1col);
+    sessionStorage.setItem("player2-col", p2col);
 });
-
-
 
 });
--- JS/javascript.js
+++ JS/javascript.js
@@ -1,59 +1,66 @@
 ///////////// I should use comments and nice things here for my own benefit
 
 /// things that can be cleaned up - move error handling to its own function
-
 $(document).ready(function () { //////I am still not quite sure how this works. But wrapping everything in it does work...
 
 ///// Global Variables
 let naughtScore = 0;
 let crossScore = 0;
-let currentPlayer = "O"; //this is currently redundant
+let currentPlayer = "";
 let isPOne = true;
+let player1name = sessionStorage.getItem("player1-name");
+let player2name = sessionStorage.getItem("player2-name");
+let player1col = sessionStorage.getItem("player1-col");
+let player2col = sessionStorage.getItem("player2-col");
+console.log(player1name)
+console.log(player2name)
+console.log(player1col)
+console.log(player2col)
 
-$('#error-message').hide()
+// let isGameComplete = false;
+$('#announcement-message').hide()
 
-/////////////// on click logic for game /////////////////
+//<<<<<<<<<<<<<<<<<<<< on click log ic for game >>>>>>>>>>>>>>>>>>>>\\
 
 $('.game-button').on('click', function(){
 $('.game-box').removeClass('animate__animated animate__shakeX');
     if (isPOne === false){
         $(this).parent().addClass('animate__animated animate__shakeX');
-        $('#error-message').show()
+        $('#announcement-message').show()
         return false
     }
-    if (($(this).html() === 'X') || ($(this).html() === 'O') ) {
-        $('#error-message').html('This square is unavailable').show()
+    if (($(this).html() === player1name) || ($(this).html() === player2name) ) {
+        $('#announcement-message').html('This square is unavailable').show()
         return false
     }
-    $(this).html("O").addClass('player1');
+    $(this).html(player1name).addClass('player1');
     isPOne = false;
     $('#player').addClass('player2') ///// These should really be a seperate function
     $('#player').removeClass('player1')
     $('#player').html("Player 2's")
-    $('#error-message').hide()
+    $('#announcement-message').hide()
     $(this).parent().removeClass('animate__animated animate__shakeX')
-    console.log(isPOne)
     winChecker();
-
 });
+
+//<<<<<<<<<<<<<<<<<<<< on right-click logic for game >>>>>>>>>>>>>>>>>>>>\\
 $('.game-button').on('contextmenu', function(){
-    $('game-box').removeClass('animate__animated animate__shakeX');
+    $('.game-box').removeClass('animate__animated animate__shakeX');
     if (isPOne === true){
         $(this).parent().addClass('animate__animated animate__shakeX')
-        $('#error-message').show()
+        $('#announcement-message').show()
         return false
     }
-    if (($(this).html() === 'X') || ($(this).html() === 'O') ) {
-        $('#error-message').html('This square is unavailable').show()
+        if (($(this).html() === player1name) || ($(this).html() === player2name) ) {
+        $('#announcement-message').html('This square is unavailable').show()
         return false
     }
-    $(this).html('X').addClass('player2')
+    $(this).html(player2name).addClass('player2')
     isPOne = true;
     $('#player').addClass('player1');     ///// These should really be a seperate function
     $('#player').removeClass('player2');
     $('#player').html("Player 1's")
-    $('#error-message').hide()
-    console.log(isPOne)
+    $('#announcement-message').hide()
     winChecker();
     return false
 });
@@ -62,17 +69,14 @@ const winChecker = function(){
     //// Checks Horizontal Wins
     if (($('#00').html() == $('#01').html() && $('#01').html() == $('#02').html()) || ($('#10').html() == $('#11').html() && $('#11').html() == $('#12').html()) || ($('#20').html() == $('#21').html() && $('#21').html() == $('#22').html())){
         scoreUpdate();
-        alert("You Win Horizontally!");
     }   
     //// Checks Vertical Wins
     if (($('#00').html() == $('#10').html() && $('#10').html() == $('#20').html()) || ($('#01').html() == $('#11').html() && $('#11').html() == $('#21').html()) || ($('#02').html() == $('#12').html() && $('#12').html() == $('#22').html())){
         scoreUpdate();
-        alert("You Win Vertically!");
     }
     //// Checks Diagonal Wins
     if (($('#00').html() == $('#11').html() && $('#11').html() == $('#22').html()) || ($('#02').html() == $('#11').html() && $('#11').html() == $('#20').html())){
         scoreUpdate();
-        alert("You Win Diagonally!");
     }
 }
 
@@ -80,10 +84,30 @@ const scoreUpdate = function() {
     if (isPOne === false) { /////////// three equals === very important
         naughtScore += 1;
         $('#naughtScore').html(naughtScore);
-    } if (isPOne === true)  {
+        currentPlayer = "Player 1";
+        $('#announcement-message').html(`${currentPlayer} has won!`).show();
+        resetFunction();
+    } if (isPOne === true) {
         crossScore += 1;
         $('#crossScore').html(crossScore);
+        currentPlayer = "Player 2";
+        $('#announcement-message').html(`${currentPlayer} has won!`).show();
+        resetFunction();
     }
+}
+
+const resetFunction = function(){
+    $('#00').html("00");
+    $('#01').html("01");
+    $('#02').html("02");
+    $('#10').html("10");
+    $('#11').html("11");
+    $('#12').html("12");
+    $('#20').html("20");
+    $('#21').html("21");
+    $('#22').html("22");
+    $('.game-button').removeClass('player1');
+    $('.game-button').removeClass('player2');
 }
 
 // <<<<<<<<<<<<<<<<<<<<<<<<<this breaks the boolean for some reason>>>>>>>>>>>>>>>>>>>>>>>>>
@@ -94,26 +118,5 @@ const scoreUpdate = function() {
 //         $('#player').addClass('player2')
 //     }
 // }
-
-const animateCSS = (element, animation, prefix = 'animate__') =>
-  // We create a Promise and return it
-  new Promise((resolve, reject) => {
-    const animationName = `${prefix}${animation}`;
-    const node = document.querySelector(element);
-
-    node.classList.add(`${prefix}animated`, animationName);
-
-    // When the animation ends, we clean the classes and resolve the Promise
-    function handleAnimationEnd(event) {
-      event.stopPropagation();
-      node.classList.remove(`${prefix}animated`, animationName);
-      resolve('Animation ended');
-    }
-
-    node.addEventListener('animationend', handleAnimationEnd, {once: true});
-  });
-
-
-
 
 });
